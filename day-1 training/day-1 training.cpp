2f6ea99a8579bef1ec8adb1d18615c5aff55bfc9
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-07 22:06:48 +0400

    print out on console homework

--- day-1 training/day-1 training.cpp
+++ day-1 training/day-1 training.cpp
@@ -1,10 +1,17 @@
-
 #include <iostream>
 
-int main()
+int main() 
 {
-    std::cout << " Hello Mohammad Aburub\n";
+	
+	std::cout << "I love programming!\n\n";
 
-    return 0;
+	std::cout << "I promise to be the best developer ever!\n\n";
+
+	std::cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+
+	std::cout << "Best Regards.\n";
+
+	std::cout << "Mohammad Aburub\n";
+
+	return 0;
 }
-
