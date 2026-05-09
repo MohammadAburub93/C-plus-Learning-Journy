d25580486e4f6242473a54617181fd91f8a6ffdf
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-09 17:22:43 +0400

    Constant decleration

--- day-3 training/day-3 training.cpp
+++ day-3 training/day-3 training.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	char char1 = 'A', char2 = 'B', char3 = 'C';
+	const int min_per_hour = 60;
 
-	cout << char1 << char2 << char3 << " reversed is " << char3 << char2 << char1 << endl;
+	cout << min_per_hour;
 
 	return 0;
 }
