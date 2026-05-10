e774e8c0c279e0f5dce74f262b8fdc02e98d2ae8
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-10 09:28:39 +0400

    redo the homework with using user input

--- homework 1/homework 1.cpp
+++ homework 1/homework 1.cpp
@@ -5,18 +5,30 @@ int main()
 {
 	string name, city, country;
 	int age;
-	float monthly_salary, yearly_salary;
+	float monthly_salary;
 	char gender;
 	bool marital_s;
 
-	name = "Mohammad Aburub";
-	age = 33;
-	city = "Amman";
-	country = "Jordan";
-	monthly_salary = 1000;
-	yearly_salary = monthly_salary * 12;
-	gender = 'M';
-	marital_s = true;
+	cout << "Please enter your name?" << endl;
+	cin >> name;
+
+	cout << "Please enter your age in numbers?" << endl;
+    cin >> age;
+
+	cout << "Please enter your city name?" << endl;
+	cin >> city;
+
+	cout << "Please enter your country?" << endl;
+	cin >> country;
+
+	cout << "Please enter your monthly salary?" << endl;
+	cin >> monthly_salary;
+
+	cout << "Please enter your gender as M for male and F for female?" << endl;
+	cin >> gender;
+
+	cout << "Please enter your marital status as 1 for married and 0 for single?" << endl;
+	cin >> marital_s;
 
 	cout << "******************************\n\n";
 	cout << "Name: " << name << "." << endl;
@@ -24,7 +36,7 @@ int main()
 	cout << "City: " << city << "." << endl;
 	cout << "Country: " << country << "." << endl;
 	cout << "Monthly Salary: " << monthly_salary << endl;
-	cout << "Yearly Salary: " << yearly_salary << endl;
+	cout << "Yearly Salary: " << monthly_salary * 12 << endl;
 	cout << "Gender: " << gender << endl;
 	cout << "Married: " << marital_s << endl << endl;
 	cout << "******************************\n\n";
