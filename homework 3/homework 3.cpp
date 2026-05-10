1cf723127d2f8762f988147f7c0c686d9925a042
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-10 09:40:30 +0400

    redo the homework using user input

--- homework 3/homework 3.cpp
+++ homework 3/homework 3.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-	int age = 25;
+	int age;
+
+	cout << "Please enter your age?\n";
+	cin >> age;
 
 	cout << "After 5 years you will be " << age + 5 << " years old.";
 
