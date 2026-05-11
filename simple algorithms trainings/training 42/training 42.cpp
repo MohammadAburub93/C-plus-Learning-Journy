34e57c754c12b80265620cb9591a124eed18ea0d
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-11 21:07:01 +0400

    Calculate number of second

---
+++ simple algorithms trainings/training 42/training 42.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	short int days, hours, min, sec;
+	int total_sec;
+
+	cout << "Please enter the number of days.\n";
+	cin >> days;
+
+	cout << "Please enter the number of hours.\n";
+	cin >> hours;
+
+	cout << "Please enter the number of minutes.\n";
+	cin >> min;
+
+	cout << "Please enter the number of seconds.\n";
+	cin >> sec;
+
+	total_sec = (days * 86400) + (hours * 3600) + (min * 60) + sec;
+
+	cout << "The total second of your task is: " << total_sec << endl;
+
+
+	return 0;
+}
