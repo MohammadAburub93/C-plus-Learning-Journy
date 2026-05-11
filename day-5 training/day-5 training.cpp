fd5c4d9c0f8ee33e02446e8422e911e27ae54706
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-11 16:42:09 +0400

    redo the code using user input

--- day-5 training/day-5 training.cpp
+++ day-5 training/day-5 training.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
-	int a = 10, b = 20;
+	int a, b;
+
+	cout << "Please enter the value of the first number a?" << endl;
+	cin >> a;
+
+	cout << "Please enter the value of the second number b?" << endl;
+	cin >> b;
 
 	cout << "a + b = " << a + b << endl;
 	cout << "a - b = " << a - b << endl;
