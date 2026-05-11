fef19ad04e2b02b523918acbe8854babcf39dc9e
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-11 17:00:51 +0400

    calculate the sum of numbers

---
+++ simple algorithms trainings/training 9/training 9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, c;
+
+	cout << "Please enter the value of first number a?" << endl;
+	cin >> a;
+
+	cout << "Please enter the value of second number b?" << endl;
+	cin >> b;
+
+	cout << "Please enter the value of third number c?" << endl;
+	cin >> c;
+
+	cout << "The sum of all numbers is: " << a + b + c << endl;
+
+	return 0;
+}
