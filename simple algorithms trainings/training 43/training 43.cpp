7501ea96ccde17ba8a8a950c5c6fae2c17c0f994
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-11 21:27:56 +0400

    Convert second into other types of time measure

---
+++ simple algorithms trainings/training 43/training 43.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int total_sec, days, hours, min, sec, remainder_sec;
+
+	cout << "Please enter the total value of seconds." << endl;
+	cin >> total_sec;
+
+	days = total_sec / 86400;
+	remainder_sec = total_sec - (days * 86400);
+
+	hours = remainder_sec / 3600;
+	remainder_sec = remainder_sec - (hours * 3600);
+
+	min = remainder_sec / 60;
+	remainder_sec = remainder_sec - (min * 60);
+
+	sec = remainder_sec;
+
+	cout << "This value of second in form of days:hours:minutes:seconds is: " << days << ":" << hours << ":" << min << ":" << sec << endl;
+	
+
+	return 0;
+}
