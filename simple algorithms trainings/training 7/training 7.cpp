777ece2f594f5f91776b250b4f69a8fcf9575c55
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-11 16:54:38 +0400

    find the half value of a number

---
+++ simple algorithms trainings/training 7/training 7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A;
+
+	cout << "Please enter the value of the number A as Integer?" << endl;
+	cin >> A;
+
+	cout << "The half of that number is " << A / 2 << endl;
+
+	return 0;
+}
