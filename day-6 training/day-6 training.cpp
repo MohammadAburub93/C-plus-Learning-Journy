473e9eba909e8df86950819cbcbdd72495aeca00
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-12 20:12:25 +0400

    Logical operators practice

--- day-6 training/day-6 training.cpp
+++ day-6 training/day-6 training.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 int main()
 {
-	short A = 10, B = 20;
+	bool result;
 
-	cout << (A == B) << endl;
-	cout << (A != B) << endl;
-	cout << (A > B) << endl;
-	cout << (A < B) << endl;
-	cout << (A >= B) << endl;
-	cout << (A <= B) << endl;
- 
+	//NOT(5 > 6 OR 7=7) AND NOT(1 OR false)
+
+	result = !(5 > 6 || 7 == 7) && !(1 || false);
+
+	cout << result << endl;
+
 	return 0;
 }
