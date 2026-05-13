49ff3f9c187d5f8ad3b47d8d15ec90b0a80b8d6b
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-13 16:52:43 +0400

    math functions in C++

--- day-6 training/day-6 training.cpp
+++ day-6 training/day-6 training.cpp
@@ -3,13 +3,7 @@ using namespace std;
 
 int main()
 {
-	bool result;
-
-	//NOT(5 > 6 OR 7=7) AND NOT(1 OR false)
-
-	result = !(5 > 6 || 7 == 7) && !(1 || false);
-
-	cout << result << endl;
+	cout << (7 == 7) && !(7 < 5); 
 
 	return 0;
 }
---
+++ day-7 training/day-7 training.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << round(2.4) << endl;
+	cout << round(2.5) << endl;
+	cout << round(2.7) << endl;
+
+	cout << sqrt(50) << endl;
+	cout << round(sqrt(50)) << endl;
+
+	return 0;
+}
