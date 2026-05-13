26da35cd957b0c2727e77237dae863d271c99774
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-13 21:14:10 +0400

    Use structure data type to define variables

--- homework 1/homework 1.cpp
+++ homework 1/homework 1.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main()
+struct p_info
 {
-	string name, city, country;
+	string name;
+	string city;
+	string country;
 	short int age;
 	float monthly_salary;
 	char gender;
 	bool marital_s;
+};
+
+int main()
+{
+	p_info person_1;
 
 	cout << "Please enter your name?" << endl;
-	cin >> name;
+	cin >> person_1.name;
 
 	cout << "Please enter your age in numbers?" << endl;
-    cin >> age;
+    cin >> person_1.age;
 
 	cout << "Please enter your city name?" << endl;
-	cin >> city;
+	cin >> person_1.city;
 
 	cout << "Please enter your country?" << endl;
-	cin >> country;
+	cin >> person_1.country;
 
 	cout << "Please enter your monthly salary?" << endl;
-	cin >> monthly_salary;
+	cin >> person_1.monthly_salary;
 
 	cout << "Please enter your gender as M for male and F for female?" << endl;
-	cin >> gender;
+	cin >> person_1.gender;
 
 	cout << "Please enter your marital status as 1 for married and 0 for single?" << endl;
-	cin >> marital_s;
+	cin >> person_1.marital_s;
 
 	cout << "******************************\n\n";
-	cout << "Name: " << name << "." << endl;
-	cout << "Age: " << age << " Years." << endl;
-	cout << "City: " << city << "." << endl;
-	cout << "Country: " << country << "." << endl;
-	cout << "Monthly Salary: " << monthly_salary << endl;
-	cout << "Yearly Salary: " << monthly_salary * 12 << endl;
-	cout << "Gender: " << gender << endl;
-	cout << "Married: " << marital_s << endl << endl;
+	cout << "Name: " << person_1.name << "." << endl;
+	cout << "Age: " << person_1.age << " Years." << endl;
+	cout << "City: " << person_1.city << "." << endl;
+	cout << "Country: " << person_1.country << "." << endl;
+	cout << "Monthly Salary: " << person_1.monthly_salary << endl;
+	cout << "Yearly Salary: " << person_1.monthly_salary * 12 << endl;
+	cout << "Gender: " << person_1.gender << endl;
+	cout << "Married: " << person_1.marital_s << endl << endl;
 	cout << "******************************\n\n";
 
 	return 0;
