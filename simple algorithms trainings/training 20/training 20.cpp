eefbfceace7fd4cadacaaf1f2c1e5007e100d17e
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-13 18:16:41 +0400

    use math functions

--- simple algorithms trainings/training 20/training 20.cpp
+++ simple algorithms trainings/training 20/training 20.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
@@ -9,9 +11,9 @@ int main()
 	cout << "Please enter the square side length." << endl;
 	cin >> a;
 
-	area = (PI * a * a) / 4;
+	area = (PI * pow(a, 2)) / 4;
 
-	cout << "The area of the circle is: " << area << endl;
+	cout << "The area of the circle is: " << ceil(area) << endl;
 
 	return 0;
 
