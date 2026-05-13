c4cb1b6e0a7fafed58f44fafb911b71f90ec5d2f
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-13 21:05:41 +0400

    user defined structure data type

--- day-7 training/day-7 training.cpp
+++ day-7 training/day-7 training.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
+struct st_owner
+{
+	string full_name;
+	string phone;
+};
+
+struct car
+{
+	string brand;
+	string model;
+	short int year;
+	st_owner owner;
+};
+
 int main()
 {
-	
-	cout << "The absolute value of -10 is: " << abs(-10) << endl;
-	cout << "The absolute value of 10 is: " << abs(10) << endl;
+	car my_car_1, my_car_2;
+
+	my_car_1.brand = "BMW";
+	my_car_1.model = "X5";
+	my_car_1.year = 2000;
+	my_car_1.owner.full_name = "Mohammad Aburub";
+	my_car_1.owner.phone = "0123456789";
+
+	my_car_2.brand = "Ford";
+	my_car_2.model = "Mustang";
+	my_car_2.year = 2022;
+	my_car_2.owner.full_name = "Mohammad Aburub";
+	my_car_2.owner.phone = "0123456789";
+
+	cout << my_car_1.brand << " " << my_car_1.model << " " << my_car_1.year << " " << my_car_1.owner.full_name << endl;
+	cout << my_car_2.brand << " " << my_car_2.model << " " << my_car_2.year << " " << my_car_2.owner.phone << endl;
+
+
+
 
 
 	return 0;
