062f81bef9fd5e0b99d5e20b30dbccbcf6810ccb
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-14 18:32:26 +0400

    Nested structures and enums in C++

--- day-8 training/day-8 training.cpp
+++ day-8 training/day-8 training.cpp
@@ -2,26 +2,56 @@
 
 using namespace std;
 
-enum color {red, green, yellow, blue };
-enum direction { north, south, east, west };
-enum week { Sat, Sun, Mon, Tue , Wed, Thu, Fri };
-enum gender { male, female};
-enum status {single, married};
+enum enColor {red, green, yellow, blue };
+enum enGender { male, female};
+enum enStatus {single, married};
+
+struct st_address
+{
+	string streetname;
+	string building_num;
+	string POBox;
+	string zipcode;
+};
+
+struct st_contact_info
+{
+	string phone;
+	string email;
+	st_address address;
+};
+
+struct st_person
+{
+	string first_name;
+	string last_name;
+
+	st_contact_info contact_info;
+
+	enStatus marital_status;
+	enGender gender;
+	enColor fav_color;
+};
 
 int main()
 {
-	color my_color;
-	direction my_direction;
-	week today;
-	status my_status;
+	st_person person_1;
 
-	my_color = color::blue;
-	my_direction = direction::west;
-	today = week::Thu;
-	my_status = status::single;
+	person_1.first_name = "Mohammad";
+	person_1.last_name = "Aburub";
+	person_1.contact_info.email = "mmmmmmmm@gmail.com";
+	person_1.contact_info.phone = "0123456789";
+	person_1.contact_info.address.building_num = "5";
+	person_1.contact_info.address.POBox = "00000";
+	person_1.contact_info.address.streetname = "Mohammad";
+	person_1.contact_info.address.zipcode =  "00000";
 
-	cout << "Color is: " << my_color << endl;
+	person_1.gender = enGender::male;
+	person_1.fav_color = enColor::blue;
+	person_1.marital_status = enStatus::single;
 
+	cout << person_1.first_name << " " << person_1.last_name << endl;
+	cout << person_1.contact_info.phone << endl;
 
 	return 0;
 }
