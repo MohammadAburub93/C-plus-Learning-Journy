83bd1e8645efddaad9d4ae0e9c7ab6e12db21c08
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-16 20:34:04 +0400

    data type convertion

---
+++ day-9 training/day-9 training.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str = "123.456";
+
+	int num_int = stoi(str);
+
+	float num_float = stof(str);
+
+	double num_double = stod(str);
+
+	cout << num_int << endl;
+	cout << num_float << endl;
+	cout << num_double << endl;
+
+
+	return 0;
+}
