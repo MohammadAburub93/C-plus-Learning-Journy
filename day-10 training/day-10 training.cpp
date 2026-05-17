62ab5e57358e229a466d163f4c089088ba24b1ea
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-17 17:27:15 +0400

    strings homework

--- day-10 training/day-10 training.cpp
+++ day-10 training/day-10 training.cpp
@@ -5,22 +5,24 @@ using namespace std;
 
 int main()
 {
-	string my_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string str1, str2, str3;
+	int result;
 
-	cout << "The length of my string is: " << my_string.length() << endl;
+	cout << "Please enter string 1 value?" << endl;
+	getline(cin, str1);
 
-	cout << my_string[1] << endl;
+	cout << endl << "Please enter string 2 value?" << endl;
+	cin >> str2;
 
-	string s1 = "10", s2 = "20";
+	cout << endl << "Please enter string 3 value?" << endl;
+	cin >> str3;
 
-	string s3 = s1 + s2;
+	cout << "***********************************" << endl;
+	cout << "The length of string 1 is: " << str1.length() << endl;
+	cout << "Characters at locations 0, 2, 4, 7 are: " << str1[0] << " " << str1[2] << " " << str1[4] << " " << str1[7] << endl;
+	cout << "Concatenating string 2 and string 3 = " << str2 + str3 << endl;
+	cout << str2 << " * " << str3 << " = " << stoi(str2) * stoi(str3) << endl;
 
-	cout << s3 << endl;
 
-	int sum = stoi(s1) + stoi(s2);
-
-	cout << sum << endl;
-
-	
 	return 0;
 }
