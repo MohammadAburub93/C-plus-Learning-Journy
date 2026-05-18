8a1dfe6b63ed9545cc2011e208a91ab126b58bac
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-18 20:25:09 +0400

    use cin.ignore

--- day-11 training/day-11 training.cpp
+++ day-11 training/day-11 training.cpp
@@ -3,23 +3,22 @@
 
 using namespace std;
 
-void my_function()
-{
-	cout << "This is my first C++ function, it has been executed :-)" << endl;
-}
-
-void my_function2()
-
-{
-	cout << "This is my second function in C++ :-)" << endl;
-}
-
-
-
 int main()
 {
-	my_function();
-	my_function2();
+	int number;
+	string name, country;
+
+	cout << "Please enter emplyee number?\n";
+	cin >> number;
+
+	cout << "Please enter name?\n";
+	cin.ignore(1, '\n');
+	getline(cin, name);
+
+	cout << "Please enter country?\n";
+	cin >> country;
+
+	cout << "Number: " << number << ", Name: " << name << ", Country: " << country << endl;
 
 	return 0;
 }
