92247366dd96081b9923a846f03581c537eec27d
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-18 19:30:00 +0400

    functions in C++

---
+++ day-11 training/day-11 training.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void my_function()
+{
+	cout << "This is my first C++ function, it has been executed :-)" << endl;
+}
+
+void my_function2()
+
+{
+	cout << "This is my second function in C++ :-)" << endl;
+}
+
+
+
+int main()
+{
+	my_function();
+	my_function2();
+
+	return 0;
+}
