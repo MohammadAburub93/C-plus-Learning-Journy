236adf0f8837ded453316ab5a1fabecb9622a6dd
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-19 20:29:59 +0400

    use function and parameters

--- simple algorithms trainings/training 16/training 16.cpp
+++ simple algorithms trainings/training 16/training 16.cpp
@@ -3,10 +3,14 @@
 
 using namespace std;
 
+double rec_area(float s_length, float hyp_length)
+{
+	return s_length* (sqrt(pow(hyp_length, 2) - pow(s_length, 2)));
+}
+
 int main()
 {
 	float s_length, hyp_length;
-	double area;
 
 	cout << "Please enter the value of the rectangle side length?" << endl;
 	cin >> s_length;
@@ -14,9 +18,8 @@ int main()
 	cout << "Please enter the value of the hypotenous length?" << endl;
 	cin >> hyp_length;
 
-	area = s_length * (sqrt(pow(hyp_length, 2) - pow(s_length, 2)));
 
-	cout << "The area of the rectangle is: " << area << endl;
+	cout << "The area of the rectangle is: " << rec_area(s_length, hyp_length) << endl;
 
 	return 0;
 }
