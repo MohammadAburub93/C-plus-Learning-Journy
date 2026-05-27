e17b34ce465f3e3c0caeee7b6f775d2a1c3cd25b
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-05-28 00:40:29 +0400

    use function and parameters

--- simple algorithms trainings/training 23/training 23.cpp
+++ simple algorithms trainings/training 23/training 23.cpp
@@ -3,11 +3,23 @@
 
 using namespace std;
 
+const float PI = 3.14;
+
+float area_calc(float a, float b, float c)
+{
+	float p;
+	p = (a + b + c) / 2;
+
+	return (PI * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2));
+
+
+}
+
 int main()
 {
 
-	float a, b, c, p, area;
-	const float PI = 3.14;
+	float a, b, c, area;
+	
 
 	cout << "Please enter the first triangle side length?" << endl;
 	cin >> a;
@@ -18,9 +30,9 @@ int main()
 	cout << "Please enter the third triangle side length?" << endl;
 	cin >> c;
 
-	p = (a + b + c) / 2;
+	
 
-	area = PI * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2);
+	area = area_calc(a, b, c);
 
 	cout << "The area of the circle is: " << ceil(area) << endl;
 
