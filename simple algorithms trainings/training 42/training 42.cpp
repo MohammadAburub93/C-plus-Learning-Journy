9197d699ce0969a052154469d4fa9a759bb4a197
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-03 16:36:18 +0400

    use function and parameters

--- simple algorithms trainings/training 42/training 42.cpp
+++ simple algorithms trainings/training 42/training 42.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+int sec_calc(short days, short hours, short min, short sec)
+{
+	return ((days * 86400) + (hours * 3600) + (min * 60) + sec);
+}
+
+
 int main()
 {
 
@@ -21,7 +27,7 @@ int main()
 	cout << "Please enter the number of seconds.\n";
 	cin >> sec;
 
-	total_sec = (days * 86400) + (hours * 3600) + (min * 60) + sec;
+	total_sec = sec_calc(days, hours, min, sec);
 
 	cout << "The total second of your task is: " << round(total_sec) << endl;
 
