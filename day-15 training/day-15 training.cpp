1d879819de6a3a2966610e65bdf48d95a7a3d3d0
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-11 20:34:58 +0400

    using arrays

---
+++ day-15 training/day-15 training.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x[5] = { 22, 18, 2, 55, 520 };
+
+	cout << x[0] << endl;
+	cout << x[1] << endl;
+	cout << x[2] << endl;
+	cout << x[3] << endl;
+	cout << x[4] << endl;
+
+	cout << x[0] + x[4] << endl;
+
+	return 0;
+
+}
