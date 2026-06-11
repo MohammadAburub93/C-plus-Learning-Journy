361c6b697d74c69461763a7362ba36b1ee44a65f
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-11 20:42:33 +0400

    initialization values after the variable

--- day-15 training/day-15 training.cpp
+++ day-15 training/day-15 training.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-	int x[5] = { 22, 18, 2, 55, 520 };
+	int x[5];
+
+	x[0] = 10;
+	x[1] = 20;
 
 	cout << x[0] << endl;
-	cout << x[1] << endl;
-	cout << x[2] << endl;
-	cout << x[3] << endl;
-	cout << x[4] << endl;
+	cout << x[1] << endl;;
 
-	cout << x[0] + x[4] << endl;
+	cout << x[0] + x[1] << endl;
 
 	return 0;
 
