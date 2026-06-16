1385fe57ef19247394b1b5a96fde80a2bf0f3052
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-16 16:07:02 +0400

    if statement

---
+++ day-17 training/day-17 training.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+
+	if (x > 5)
+	{
+		cout << "The code of 'if' body has executed." << endl;
+	}
+
+	cout << "The code after 'if' body always executed." << endl;
+
+	return 0;
+}
