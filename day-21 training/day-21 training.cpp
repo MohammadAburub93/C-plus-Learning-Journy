9117c1775e2ca52f133c5b2a2f6e264277a16323
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-20 12:04:33 +0400

    while loop training

--- day-21 training/day-21 training.cpp
+++ day-21 training/day-21 training.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 
+int read_number_in_range(int from, int to)
+{
+	int num;
+
+	cout << "Please enter a number between " << from << " to " << to << " ?" << endl;
+	cin >> num;
+
+	while (num < from || num > to)
+	{
+		cout << "It's a wrong number, ";
+		cout << "Please enter a number between " << from << " to " << to << " ?" << endl;
+		cin >> num;
+	}
+
+	return num;
+}
+
 int main()
 {
-	for (int i = 65; i <= 70; i++)
-	{
-		for (int j = 65; j <= i; j++)
-		{
-			cout << char(j) << " ";
-		}
-
-		cout << "\n";
-	}
+	
+	cout << "\nThe number is: " << read_number_in_range(0, 100) << endl;
 
 	return 0;
 }
