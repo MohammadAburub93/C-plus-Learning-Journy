9ad0154445b3f39ff27bfa69407f048603cb1a91
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-20 06:27:48 +0400

    alphabet repetetion

--- day-21 training/day-21 training.cpp
+++ day-21 training/day-21 training.cpp
@@ -4,15 +4,12 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 10; i++)
+	for (int i = 65; i <= 90; i++)
 	{
-
-		for (int j = 1; j <= i; j++)
+		for (int j = 65; j <= 90; j++)
 		{
-			cout << "*";
+			cout << char(i) << char(j) << endl;
 		}
-		
-		cout << endl;
 	}
 
 	return 0;
