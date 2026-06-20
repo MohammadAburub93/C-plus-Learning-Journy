7055f5d1e0d6127789777bf9c2768fc31a659980
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-20 06:36:07 +0400

    ascending characters printing

--- day-21 training/day-21 training.cpp
+++ day-21 training/day-21 training.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i <= 10; i++)
+	for (int i = 65; i <= 70; i++)
 	{
-		for (int j = 1; j <= i; j++)
+		for (int j = 65; j <= i; j++)
 		{
-			cout << j << " ";
+			cout << char(j) << " ";
 		}
 
 		cout << "\n";
