914ebdad2f96512a8bc58bee52fce965cd65cbbe
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-20 19:24:33 +0400

    Continue statement

--- day-21 training/day-21 training.cpp
+++ day-21 training/day-21 training.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 int main()
 {
-	int arr[10] = { 10, 20, 44, 55, 33, 22, 99, 88, 99, 100 };
-	int search_value = 44;
-
-	for (int i = 0; i < 10; i++)
+	int sum = 0;
+	int num;
+	
+	for (int i = 1; i <= 5; i++)
 	{
-		cout << "We are now at position " << i << endl;
-		if (arr[i] == search_value)
-		{
-			cout << "The value of " << search_value << " is found at position " << i << endl;
-			break;
-		}
+		cout << "Please enter thr number?\n";
+		cin >> num;
 
+		if (num >= 50)
+		{
+			continue;
+		}
+		sum += num;
 	}
+
+	cout << "The total sum is: " << sum << endl;
 
 	return 0;
 }
