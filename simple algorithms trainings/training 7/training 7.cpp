f58df1e8f231f7ea043715d905c63a91a40fa261
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-21 11:24:48 +0400

    use functions

--- simple algorithms trainings/training 7/training 7.cpp
+++ simple algorithms trainings/training 7/training 7.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int read_number()
 {
-	int A;
+	int num;
 
 	cout << "Please enter the value of the number A as Integer?" << endl;
-	cin >> A;
+	cin >> num;
 
-	cout << "The half of that number is " << A / 2 << endl;
+	return num;	
+}
+
+float half_calc(int num)
+{
+	return (float)num / 2;
+}
+
+void print_half_val(int num)
+{
+	cout << "The half of that number is " << half_calc(num) << endl;
+}
+int main()
+{
+	print_half_val(read_number());
 
 	return 0;
 }
