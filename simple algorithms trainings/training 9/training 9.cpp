94b80ed011ded2e1261dc9b2b47b39433558caa7
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-21 12:34:27 +0400

    code organizing

--- simple algorithms trainings/training 9/training 9.cpp
+++ simple algorithms trainings/training 9/training 9.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void read_numbers(float &num1, float &num2, float &num3)
+{
+
+	cout << "Please enter the value of first number ?" << endl;
+	cin >> num1;
+
+	cout << "Please enter the value of second number ?" << endl;
+	cin >> num2;
+
+	cout << "Please enter the value of third number ?" << endl;
+	cin >> num3;
+
+}
+
+float number_sum(float num1, float num2, float num3)
+{
+	float result;
+
+	result = num1 + num2 + num3;
+
+	return result;
+}
+
+void print_sum(float result)
+{
+	cout << "The sum of all numbers is: " << result << endl;
+}
+
 int main()
 {
-	float a, b, c;
+	float num1, num2, num3;
 
-	cout << "Please enter the value of first number a?" << endl;
-	cin >> a;
-
-	cout << "Please enter the value of second number b?" << endl;
-	cin >> b;
-
-	cout << "Please enter the value of third number c?" << endl;
-	cin >> c;
-
-	cout << "The sum of all numbers is: " << a + b + c << endl;
+	read_numbers(num1, num2, num3);
+	print_sum(number_sum(num1, num2, num3));
 
 	return 0;
 }
