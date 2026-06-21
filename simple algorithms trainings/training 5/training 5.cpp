758dd317dc88a3578b40aaaa3d77e312f466dd74
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-21 09:36:14 +0400

    hire employee with recommendation

---
+++ simple algorithms trainings/training 5/training 5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+struct emp_info
+{
+	int age;
+	bool has_d_license;
+	bool has_recommendation;
+};
+
+emp_info read_emp_info()
+{
+	emp_info person;
+
+	cout << "please enter your age?\n";
+	cin >> person.age;
+
+	cout << "do you have a driver license? please enter 1 for yes and 0 for no.\n";
+	cin >> person.has_d_license;
+
+	cout << "do you have a recommendation? please enter 1 for yes and 0 for no.\n";
+	cin >> person.has_d_license;
+
+	return person;
+}
+
+bool is_accepted(emp_info person)
+{
+	return ((person.age > 21 && person.has_d_license) || person.has_recommendation);
+}
+
+void print_result(emp_info person)
+{
+	if (is_accepted(person))
+
+		cout << "\nHired" << endl;
+	else
+		cout << "\nRejected" << endl;
+}
+
+int main()
+{
+	print_result(read_emp_info());
+
+	return 0;
+}
