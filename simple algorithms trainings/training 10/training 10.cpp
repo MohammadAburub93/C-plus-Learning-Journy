38055beaf5923dd37db16de4c4ce2f0a7051bee9
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-21 12:47:29 +0400

    calculate average marks

--- simple algorithms trainings/training 10/training 10.cpp
+++ simple algorithms trainings/training 10/training 10.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
 using namespace std;
 
+void read_marks(short& mark1, short& mark2, short& mark3)
+{
+
+	cout << "Please enter the value of first mark ?" << endl;
+	cin >> mark1;
+
+	cout << "Please enter the value of second mark ?" << endl;
+	cin >> mark2;
+
+	cout << "Please enter the value of third mark ?" << endl;
+	cin >> mark3;
+
+}
+
+int marks_sum(short mark1, short mark2, short mark3)
+{
+	int result;
+
+	result = mark1 + mark2 + mark3;
+
+	return result;
+}
+
+float marks_average(short mark1, short mark2, short mark3)
+{
+	return (float)marks_sum(mark1, mark2, mark3) / 3;
+}
+
+void print_sum(float result)
+{
+	cout << "The average of the marks is: " << result << endl;
+}
+
 int main()
 {
-	float mark_1, mark_2, mark_3, marks_avg;
+	short mark1, mark2, mark3;
 
-	cout << "Please enter the first mark you have?" << endl;
-	cin >> mark_1;
-
-	cout << "Please enter the second mark you have?" << endl;
-	cin >> mark_2;
-
-	cout << "Please enter the third mark you have?" << endl;
-	cin >> mark_3;
-
-	marks_avg = (mark_1 + mark_2 + mark_3) / 3;
-
-	cout << "Your average will be: " << marks_avg << endl;
+	read_marks(mark1, mark2, mark3);
+	print_sum(marks_average(mark1, mark2, mark3));
 
 	return 0;
 }
