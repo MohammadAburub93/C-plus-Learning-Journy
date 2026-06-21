f8055a2e638dc5458e87ff01f3c3691aad09a90e
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-21 11:45:45 +0400

    pass fail training

--- simple algorithms trainings/training 8/training 8.cpp
+++ simple algorithms trainings/training 8/training 8.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void read_mark(float &mark)
+enum enPassFail {pass = 1, fail = 2};
+
+float read_mark()
 {
+	float mark;
+
 	cout << "Please enter your mark?" << endl;
 	cin >> mark;
+
+	return mark;
 }
 
-void check_mark(float user_mark)
+enPassFail mark_pass(float mark)
 {
-	if (user_mark >= 50)
-	{
-		cout << "Pass" << endl;
-	}
+	enPassFail result;
+	if (mark >= 50)
+		result = enPassFail::pass;
 	else
-	{
-		cout << "Fail" << endl;
-	}
+		result = enPassFail::fail;
+
+	return result;
+}
+
+void print_mark_result(float mark)
+{
+	if (mark_pass(mark) == enPassFail::pass)
+		cout << "Based on your mark ("<< mark << ") your result is: Pass" << endl;
+	else
+		cout << "Based on your mark (" << mark << ") your result is: Fail" << endl;
 }
 
 int main()
 {
 	float user_mark;
 
-	read_mark(user_mark);
-	check_mark(user_mark);
+	print_mark_result(read_mark());
 
 	return 0;
 }
