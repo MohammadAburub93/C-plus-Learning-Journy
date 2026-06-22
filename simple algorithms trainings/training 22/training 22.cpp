be011a71193348407f471e2381cfac0d3c90080d
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 16:34:03 +0400

    organize the code

--- simple algorithms trainings/training 22/training 22.cpp
+++ simple algorithms trainings/training 22/training 22.cpp
@@ -3,27 +3,38 @@
 
 using namespace std;
 
-const float PI = 3.14;
 
-float area_calc(float a, float b)
+void read_triangel_sides_length(float &a, float &b)
 {
-	return (PI * (pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b)));
-}
-
-int main()
-{
-	float a,b, area;
-	
 	cout << "Please enter the triangle side length." << endl;
 	cin >> a;
 
 	cout << "Please enter the triangle base length." << endl;
 	cin >> b;
+}
 
-	area = area_calc(a, b);
+double circle_area_calc(float a, float b)
+{
+	double area;
 
-	cout << "The area of the circle is: " << ceil(area) << endl;
+	const float PI = 3.14;
+
+	area = (PI * (pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b)));
+
+	return area;
+}
+
+void print_circle_area(float area)
+{
+	cout << "The area of the selected circle is: " << area << endl;
+}
+
+int main()
+{
+	float a, b;
+
+	read_triangel_sides_length(a, b);
+	print_circle_area(circle_area_calc(a, b));
 
 	return 0;
-
 }
