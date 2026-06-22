3b0c12e58508d960236130af91f3752b9ca3a968
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 12:07:54 +0400

    organize the code

--- simple algorithms trainings/training 18/training 18.cpp
+++ simple algorithms trainings/training 18/training 18.cpp
@@ -3,23 +3,32 @@
 
 using namespace std;
 
-const float PI = 3.14;
+
+float read_circle_radius()
+{
+	float r;
+
+	cout << "Please enter the radius value?" << endl;
+	cin >> r;
+
+	return r;
+}
 
 float area_calc(float r)
 {
+	const float PI = 3.14;
+
 	return PI * pow(r, 2);
+}
+
+void print_circle_area(float area)
+{
+	cout << "The area of the selected circle rounded up is: " << area << endl;
 }
 
 int main()
 {
-	float r, area;
-	
-	cout << "Please enter the radius value?" << endl;
-	cin >> r;
-
-
-	cout << "The area of the selected circle rounded up is: " << ceil(area_calc(r)) << endl;
-
+	print_circle_area(area_calc(read_circle_radius()));
 
 	return 0;
 }
