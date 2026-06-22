284588654773dcdf74faf90950f6b632f7356aa0
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 08:46:55 +0400

    code organizing

--- simple algorithms trainings/training 15/training 15.cpp
+++ simple algorithms trainings/training 15/training 15.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
 
-
-float rec_area(float a, float b)
+void read_rectangle_values(float &a, float &b)
 {
-	return a * b;
-}
-
-int main()
-{
-	float a, b;
-
 	cout << "Please enter the rectangle longest side length." << endl;
 	cin >> a;
 
 	cout << "Please enter the rectangle shortest side length." << endl;
 	cin >> b;
 
+}
 
-	cout << "The area of the rectangle is: " << rec_area(a, b) << endl;
+float rec_area(float a, float b)
+{
+	return a * b;
+}
+
+void print_area_value(float area)
+{
+	cout << "The area of the rectangle is: " << area << endl;
+}
+
+int main()
+{
+	float a, b;
+
+	read_rectangle_values(a, b);
+	print_area_value(rec_area(a, b));
 
 	return 0;
 }
