4a9d88354cd222373f8611d0be274c876e5082eb
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 11:54:13 +0400

    organize the code

--- simple algorithms trainings/training 16/training 16.cpp
+++ simple algorithms trainings/training 16/training 16.cpp
@@ -3,23 +3,31 @@
 
 using namespace std;
 
-double rec_area(float s_length, float hyp_length)
+void read_rec_values(float &s_length, float &hyp_length)
 {
-	return s_length* (sqrt(pow(hyp_length, 2) - pow(s_length, 2)));
-}
-
-int main()
-{
-	float s_length, hyp_length;
-
 	cout << "Please enter the value of the rectangle side length?" << endl;
 	cin >> s_length;
 
 	cout << "Please enter the value of the hypotenous length?" << endl;
 	cin >> hyp_length;
+}
 
+double rec_area(float s_length, float hyp_length)
+{
+	return s_length* (sqrt(pow(hyp_length, 2) - pow(s_length, 2)));
+}
 
-	cout << "The area of the rectangle is: " << rec_area(s_length, hyp_length) << endl;
+void print_rec_area(double area)
+{
+	cout << "The area of the rectangle is: " << area << endl;
+}
 
+int main()
+{
+	float s_length, hyp_length;
+	
+	read_rec_values(s_length, hyp_length);
+	print_rec_area(rec_area(s_length, hyp_length));
+	
 	return 0;
 }
