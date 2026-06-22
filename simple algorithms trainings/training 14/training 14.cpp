8a3fe17d4f1914cc75f3d52626c4388524e4b284
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 08:36:36 +0400

    organizing code

--- simple algorithms trainings/training 14/training 14.cpp
+++ simple algorithms trainings/training 14/training 14.cpp
@@ -2,31 +2,38 @@
 using namespace std;
 
 
-short int num_swap(short a, short b)
+void read_numbers(int &num1, int &num2)
 {
+	cout << "Please enter the first number?\n";
+	cin >> num1;
 
-	b = a;
-
-	return b;
+	cout << "Please enter the second number?\n";
+	cin >> num2;
 }
 
+void num_swap(int &num1, int &num2)
+{
+	int swap_num;
 
+	swap_num = num1;
+
+	num1 = num2;
+	num2 = swap_num;
+}
+
+void print_numbers_value(int num1, int num2)
+{
+	cout << "First number is " << num1 << " and second number is " << num2 << "\n\n";
+}
 
 int main()
 {
-	short int a, b, swapped_a, swapped_b;
+	int num1, num2;
 
-	cout << "Please enter the first number a?" << endl;
-	cin >> a;
-
-	cout << "Please enter the second number b?" << endl;
-	cin >> b;
-
-	swapped_a = b;
-	swapped_b = num_swap(a, b);
-
-	cout << "the first number you pick is: " << a << " ,and the second number is: " << b << endl;
-	cout << "after swap the first number become: " << swapped_a << ", and the second number is: " << swapped_b << endl;
+	read_numbers(num1, num2);
+	print_numbers_value(num1, num2);
+	num_swap(num1, num2);
+	print_numbers_value(num1, num2);
 
 	return 0;
 
