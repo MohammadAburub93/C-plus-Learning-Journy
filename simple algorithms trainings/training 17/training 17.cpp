07a76583380fbfebff2ea9ee080e4d026b33679f
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 12:00:06 +0400

    organize the code

--- simple algorithms trainings/training 17/training 17.cpp
+++ simple algorithms trainings/training 17/training 17.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void read_triangle_sides(float &base, float &hight)
 {
-	float base, hight, area;
-
 	cout << "Please enter the triangle base length." << endl;
 	cin >> base;
 
 	cout << "Please enter the triangle hight." << endl;
 	cin >> hight;
+}
 
-	area = 0.5 * base * hight;
+float calc_trianglr_area(float base, float hight)
+{
+	return (0.5 * base * hight);
+}
 
+void print_tri_area(float area)
+{
 	cout << "The area of the triangle is: " << area << endl;
+}
+
+int main()
+{
+	float base, hight;
+
+	read_triangle_sides(base, hight);
+	print_tri_area(calc_trianglr_area(base, hight));
 
 	return 0;
 }
