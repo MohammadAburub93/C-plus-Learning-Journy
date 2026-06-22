9e3e0041991edae2ecfc256e9c0d3a197c940a59
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-22 16:39:32 +0400

    organize the code

--- simple algorithms trainings/training 23/training 23.cpp
+++ simple algorithms trainings/training 23/training 23.cpp
@@ -3,24 +3,9 @@
 
 using namespace std;
 
-const float PI = 3.14;
 
-float area_calc(float a, float b, float c)
+void read_triangel_sides_length(float& a, float& b, float &c)
 {
-	float p;
-	p = (a + b + c) / 2;
-
-	return (PI * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2));
-
-
-}
-
-int main()
-{
-
-	float a, b, c, area;
-	
-
 	cout << "Please enter the first triangle side length?" << endl;
 	cin >> a;
 
@@ -29,13 +14,32 @@ int main()
 
 	cout << "Please enter the third triangle side length?" << endl;
 	cin >> c;
+}
 
-	
+float circle_inclused_in_triangle_area_calc(float a, float b, float c)
+{
+	float area, p;
 
-	area = area_calc(a, b, c);
+	const float PI = 3.14;
 
-	cout << "The area of the circle is: " << ceil(area) << endl;
+	p = (a + b + c) / 2;
 
+	area = (PI * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2));
+
+	return area;
+}
+
+void print_circle_area(float area)
+{
+	cout << "The area of the selected circle is: " << area << endl;
+}
+
+int main()
+{
+	float a, b, c;
+
+	read_triangel_sides_length(a, b, c);
+	print_circle_area(circle_inclused_in_triangle_area_calc(a, b, c));
 
 	return 0;
 }
