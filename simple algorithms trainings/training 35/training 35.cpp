ed5b5057eee85ccf0390942edf3d8de3b0ae1d42
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-23 15:40:39 +0400

    organize the code

--- simple algorithms trainings/training 35/training 35.cpp
+++ simple algorithms trainings/training 35/training 35.cpp
@@ -1,31 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int main()
+struct stPiggyBankContent
 {
+	int Pennies, Nickels, Dimes, Quarters, Dollars;
+};
 
-	float Pennies, Nickels, Dimes, Quarters, Dollars, total_pennies, total_dollars;
+stPiggyBankContent read_piggybank_content()
+{
+	stPiggyBankContent PiggyBankContent;
 
 	cout << "Please enter the number of Penneis you want to give." << endl;
-	cin >> Pennies;
+	cin >> PiggyBankContent.Pennies;
 
 	cout << "Please enter the number of Nickels you want to give." << endl;
-	cin >> Nickels;
+	cin >> PiggyBankContent.Nickels;
 
 	cout << "Please enter the number of Dimes you want to give." << endl;
-	cin >> Dimes;
+	cin >> PiggyBankContent.Dimes;
 
 	cout << "Please enter the number of Quarters you want to give." << endl;
-	cin >> Quarters;
+	cin >> PiggyBankContent.Quarters;
 
 	cout << "Please enter the number of Dollars you want to give." << endl;
-	cin >> Dollars;
+	cin >> PiggyBankContent.Dollars;
 
-	total_pennies = Pennies + Nickels * 5 + Dimes * 10 + Quarters * 25 + Dollars * 100;
-	total_dollars = total_pennies / 100;
+	return PiggyBankContent;
+}
 
-	cout << "You have a total of Pennies equal to: " << total_pennies << endl;
-	cout << "You have a total of Dollars equal to: " << total_dollars << endl;
+int calculate_total_pennies(stPiggyBankContent PiggyBankContent)
+{
+	int PenniesTotal = 0;
+
+	PenniesTotal = PiggyBankContent.Pennies + PiggyBankContent.Nickels * 5 +
+		PiggyBankContent.Dimes * 10 + PiggyBankContent.Quarters * 25 + PiggyBankContent.Dollars * 100;
+
+	return PenniesTotal;
+}
+
+int main()
+{
+	int TotalPennies = calculate_total_pennies(read_piggybank_content());
+	 
+	cout << "You have a total of Pennies equal to: " << TotalPennies << endl;
+	cout << "You have a total of Dollars equal to: " << (float)TotalPennies / 100 << endl;
 
 
 
