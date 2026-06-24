8efa12c58a2d846240ddb36d924baa354c23129d
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-24 19:18:44 +0400

    rename the fun

--- simple algorithms trainings/training 46/training 46.cpp
+++ simple algorithms trainings/training 46/training 46.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void print_char()
+void PrintLettersAtoZ()
 {
 	for (short i = 65; i <= 90; i++)
 	{
@@ -11,7 +11,7 @@ void print_char()
 
 int main()
 {
-	print_char();
+	PrintLettersAtoZ();
 
 	return 0;
 }
