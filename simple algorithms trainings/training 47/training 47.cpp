30bad8e8225cc647b8f216a3717e2b4c016d5d8b
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-24 19:30:44 +0400

    organize the code

--- simple algorithms trainings/training 47/training 47.cpp
+++ simple algorithms trainings/training 47/training 47.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int ReadPositiveNumber(string message)
+{
+	int number;
+
+	do
+	{
+		cout << message << endl;
+		cin >> number;
+	} while (number < 0);
+
+	return number;
+}
+
+float CalculateNumberOfMonths(int TotalLoan, int MonthlyPayment)
+{
+	return (float) TotalLoan / MonthlyPayment;
+}
 int main()
 {
-	short int t_loan, payment, t_months;
+	int TotalLoan = 0, MonthlyPayment = 0;
 
-	cout << "Please enter the total amount of loan you have." << endl;
-	cin >> t_loan;
+	TotalLoan = ReadPositiveNumber("Please enter the total amount of loan you have.");
+	MonthlyPayment = ReadPositiveNumber("Please enter the monthly payment you pay.");
 
-	cout << "Please enter the monthly payment you pay." << endl;
-	cin >> payment;
-
-	t_months = t_loan / payment;
-
-	cout << "The total number of months you have to pay is: " << t_months << " Months.";
+	cout << "The total number of months you have to pay is: " << CalculateNumberOfMonths(TotalLoan, MonthlyPayment) << " Months.\n";
 
 	return 0;
 }
