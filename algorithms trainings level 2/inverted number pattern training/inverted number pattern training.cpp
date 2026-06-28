04e8decb544cecfec918e75784d052348fd9b339
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-28 20:48:26 +0400

    inverted number pattren training

---
+++ algorithms trainings level 2/inverted number pattern training/inverted number pattern training.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+	int Number = 0;
+
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+void PrintInvertedNumberPattern(int Number)
+{
+	for (int i = Number; i > 0; i--)
+	{
+		cout << "\n";
+		for (int m = 1; m <= i; m++)
+		{
+			cout << i;
+		}
+
+	}
+	cout << "\n";
+}
+
+int main()
+{
+
+	PrintInvertedNumberPattern(ReadPositiveNumber("Please enter a positve number."));
+	return 0;
+}
