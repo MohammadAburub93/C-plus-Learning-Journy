77f980665dae77ce29b2566d755cfc9d56172903
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-30 19:46:21 +0400

    fill array with random numbers

---
+++ algorithms trainings level 2/fill array with random number training/fill array with random number training.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+	int Number;
+
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber = rand() % (To - From + 1) + From;
+
+	return RandomNumber;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& ArrayLength)
+{
+	cout << "Please enter number of elments you want to add.\n";
+	cin >> ArrayLength;
+
+	for (int i = 0; i < ArrayLength; i++)
+		arr[i] = RandomNumber(1, 100);
+}
+
+void PrintArrayElements(int arr[100], int ArrayLength)
+{
+	for (int i = 0; i < ArrayLength; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	int arr[100], ArrayLength;
+
+	srand((unsigned)time(NULL));
+
+	FillArrayWithRandomNumbers(arr, ArrayLength);
+
+	cout << "\nOriginal array: ";
+	PrintArrayElements(arr, ArrayLength);
+
+	return 0;
+}
