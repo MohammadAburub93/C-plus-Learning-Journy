ddecfcf462fecedd403ce891338801dd1dc6a17b
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-06-30 18:06:26 +0400

    read array elements training

---
+++ algorithms trainings level 2/valur repeation in array training/valur repeation in array training.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadPositiveNumber(string message)
+{
+	int Number;
+
+	do
+	{
+		cout << message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+void ReadArrayElements(int arr[100], int &ArrayLength)
+{
+	cout << "Please enter number of elments you want to add.\n";
+	cin >> ArrayLength;
+
+	cout << "\nEnter array elements: \n";
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		cout << "Elemet [" << i + 1 << "] : ";
+		cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int ElementRepeationCount(int arr[100], int ArrayLength, int ValueToCheck)
+{
+	short count = 0;
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		if (arr[i] == ValueToCheck)
+			count++;
+	}
+
+	return count;
+}
+
+void PrintElementsAndRepeationCount(int arr[100], short ElementsCount, short ValueToCheck)
+{
+	cout << "\nOriginal array: ";
+	for (int i = 0; i < ElementsCount; i++)
+		cout << arr[i] << " ";
+
+	cout << "\n\n" << ValueToCheck << " is repeated " << ElementRepeationCount(arr, ElementsCount, ValueToCheck);
+	cout << " Time(s)." << endl;
+}
+
+int main()
+{
+	int arr[100], ArrayLength, ValueToCheck; 
+
+	ReadArrayElements(arr, ArrayLength);
+
+	ValueToCheck = ReadPositiveNumber("Please enter the number you want to check");
+
+	PrintElementsAndRepeationCount(arr, ArrayLength, ValueToCheck);
+
+
+	return 0;
+}
