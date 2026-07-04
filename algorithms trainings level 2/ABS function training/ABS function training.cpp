6855cca23cd63625486eda073e587a5e1165c09f
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-07-04 16:30:31 +0400

    ABS function

---
+++ algorithms trainings level 2/ABS function training/ABS function training.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadNumber()
+{
+	int Number;
+
+	cout << "Please enter a number" << endl;
+	cin >> Number;
+
+	return Number;
+}
+
+float ABSvalue(float Number)
+{
+	if (Number < 0)
+		return Number * -1;
+	else
+		return Number;
+}
+
+int main()
+{
+	float Number = ReadNumber();
+
+	cout << "My ABS result : " << ABSvalue(Number) << endl;
+	
+	cout << "C++ ABS result : " << abs(Number) << endl;
+
+	return 0;
+}
