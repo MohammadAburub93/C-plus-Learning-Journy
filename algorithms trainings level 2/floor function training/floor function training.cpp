65acbbcf4e5f118b8459cbde54942fab7edad7db
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-07-04 21:19:56 +0400

    floor function

---
+++ algorithms trainings level 2/floor function training/floor function training.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadNumber()
+{
+	float Number;
+
+	cout << "Please enter a number" << endl;
+	cin >> Number;
+
+	return Number;
+}
+
+float MyFloor(float Number)
+{	
+	if (Number > 0)
+		return int(Number);
+	else
+		return int(Number) - 1;
+}
+
+int main()
+{
+	float Number = ReadNumber();
+
+	cout << "My ABS result : " << MyFloor(Number) << endl;
+
+	cout << "C++ ABS result : " << floor(Number) << endl;
+
+	return 0;
+}
