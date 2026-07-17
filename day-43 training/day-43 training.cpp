497c0d20f743447cf47ef7724410b6d92983aaed
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-07-17 07:23:44 +0400

    Integer formatting

---
+++ day-43 training/day-43 training.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int Page = 1, TotalPages = 10;
+
+	printf("The page number = %d \n", Page);
+	printf("You are in page %d of %d \n", Page, TotalPages);
+
+	printf("The page number = %0*d \n", 2, Page);
+	printf("The page number = %0*d \n", 3, Page);
+	printf("The page number = %0*d \n", 4, Page);
+	printf("The page number = %0*d \n", 5, Page);
+
+	int Number1 = 20, Number2 = 30;
+	printf("The result of %d + %d \n", Number1, Number2, Number1 + Number2);
+
+
+	return 0;
+}
