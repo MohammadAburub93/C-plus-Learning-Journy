cfa647f0f5b1434888a37fd6ae0cf8aade9faef7
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-07-19 09:39:45 +0400

    two dimensional array homework

--- day-45 training/day-45 training.cpp
+++ day-45 training/day-45 training.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
-	int x[3][4] = { {1,2,3,4}, {5,6,7,8},{9,10,11,12} };
+	int MulTable[10][10];
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for (int j = 0; j < 4; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			cout << x[i][j] << " ";
+			MulTable[i][j] = (i + 1) * (j + 1);
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			printf("%0*d ", 2, MulTable[i][j]);
 		}
 
 		cout << endl;
