ef0dfb318f60629dae1f561377f2c35158d9d05d
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-08-30 20:02:41 +0400

    vector iterator

--- day-50 training/day-50 training.cpp
+++ day-50 training/day-50 training.cpp
@@ -6,28 +6,11 @@ int main()
 {
 	vector<int> num{ 1, 2, 3, 4, 5 };
 	
-	cout << "Initial Vector: ";
+	vector<int>::iterator iter;
 
-	for (const int& i : num) {
-		cout << i << " ";
+	for (iter = num.begin(); iter != num.end(); iter++) {
+		cout << *iter << " ";
 	}
-
-	cout << "\n\nUpdated Vector: ";
-
-	for (int& i : num) {
-		i = 20;
-		cout << i << " ";
-	}
-
-	num[1] = 40;
-	num.at(2) = 80;
-	num.at(4) = 90;
-
-	cout << "\n\nUpdated Vector: ";
-
-	for (const int& i : num) {
-		cout << i << " ";
-	}
-
+	
 	return 0;
 }
