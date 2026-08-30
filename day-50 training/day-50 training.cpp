14609f566569152ea7e2a29898568024ef6280fc
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-08-30 19:59:09 +0400

    change vector elements

--- day-50 training/day-50 training.cpp
+++ day-50 training/day-50 training.cpp
@@ -5,18 +5,29 @@ using namespace std;
 int main()
 {
 	vector<int> num{ 1, 2, 3, 4, 5 };
+	
+	cout << "Initial Vector: ";
 
-	cout << "\n\n using .at(i) \n";
-	cout << "Element at Index 0: " << num.at(0) << endl;
-	cout << "Element at index 1: " << num.at(1) << endl;
-	cout << "Element at index 2: " << num.at(2) << endl;
-	cout << "Element at index 3: " << num.at(3) << endl;
+	for (const int& i : num) {
+		cout << i << " ";
+	}
 
-	cout << "\n\n using [i]\n";
-	cout << "Element at Index 0: " << num[0] << endl;
-	cout << "Element at Index 1: " << num[1] << endl;
-	cout << "Element at Index 2: " << num[2] << endl;
-	cout << "Element at Index 3: " << num[3] << endl;
+	cout << "\n\nUpdated Vector: ";
+
+	for (int& i : num) {
+		i = 20;
+		cout << i << " ";
+	}
+
+	num[1] = 40;
+	num.at(2) = 80;
+	num.at(4) = 90;
+
+	cout << "\n\nUpdated Vector: ";
+
+	for (const int& i : num) {
+		cout << i << " ";
+	}
 
 	return 0;
 }
