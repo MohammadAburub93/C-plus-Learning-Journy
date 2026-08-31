9b839fe7514034aa9f4dc1b13d1750cbb687e6f0
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-08-31 19:32:26 +0400

    string methods

--- day-51 training/day-51 training.cpp
+++ day-51 training/day-51 training.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-	vector<int> num{ 1, 2, 3, 4, 5 };
+	string S1 = "My name is Mohammad Aburub, Ilearn programming";
 
-	try
+	cout << S1.length() << endl;
+
+	cout << S1.at(3) << endl;
+
+	S1.append(" @ProgrammingAdvices");
+	cout << S1 << endl;
+
+	S1.insert(7, " Tawfiq ");
+	cout << S1 << endl;
+
+	cout << S1.substr(19, 8) << endl;
+
+	S1.push_back('X');
+	cout << S1 << endl;
+
+	S1.pop_back();
+	cout << S1 << endl;
+
+	cout << S1.find("Tawfiq") << endl;
+
+	cout << S1.find("tawfiq") << endl;
+
+	if (S1.find("tawfiq") == S1.npos)
 	{
-
-		cout << num.at(5);
-	}
-	catch (...)
-	{
-
-		cout << "out of bound\n";
+		cout << "tawfiq is not found";
 	}
 
+	S1.clear();
+
+	cout << S1 << endl;
 	return 0;
 }
