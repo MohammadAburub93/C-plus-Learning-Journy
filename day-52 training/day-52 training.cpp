74b0ee7f1dec28ea47fa5d52c2e3076a7058323e
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-09-01 11:28:52 +0400

    delete a record from a file

--- day-52 training/day-52 training.cpp
+++ day-52 training/day-52 training.cpp
@@ -4,39 +4,24 @@
 #include <vector>
 using namespace std;
 
-//void LoadDataFromFileToVector(string FileName, vector<string>& vFileContent)
-//{
-//	fstream File;
-//
-//	File.open(FileName, ios::in);
-//
-//	if (File.is_open())
-//	{
-//		string Line;
-//
-//		while (getline(File, Line))
-//		{
-//			vFileContent.push_back(Line);
-//		}
-//
-//		File.close();
-//	}
-//}
-//
-//
-//int main()
-//{
-//	vector<string> vFileContent;
-//
-//	LoadDataFromFileToVector("SecondFile.txt", vFileContent);
-//
-//	for (string &Line : vFileContent)
-//	{
-//		cout << Line << endl;
-//	}
-//
-//	return 0;
-//}
+void LoadDataFromFileToVector(string FileName, vector<string>& vFileContent)
+{
+	fstream File;
+
+	File.open(FileName, ios::in);
+
+	if (File.is_open())
+	{
+		string Line;
+
+		while (getline(File, Line))
+		{
+			vFileContent.push_back(Line);
+		}
+
+		File.close();
+	}
+}
 
 void SaveVectorToFile(string FileName, vector <string> vFileContent)
 {
@@ -52,19 +37,62 @@ void SaveVectorToFile(string FileName, vector <string> vFileContent)
 
 				File << Line << endl;
 			}
-			
+
 		}
 
 		File.close();
 	}
 }
 
+void RemoveRecordFromVector(string FileName, string Record)
+{
+	vector<string> vFileContent;
+
+	LoadDataFromFileToVector(FileName, vFileContent);
+
+	for (string& Item : vFileContent)
+	{
+		if (Item == Record)
+		{
+			Item = "";
+		}
+	}
+
+	SaveVectorToFile("SecondFile.txt", vFileContent);
+}
+
+
+void PrintFileContent(string FileName)
+{
+	fstream File;
+
+	File.open(FileName, ios::in);
+
+	if (File.is_open())
+	{
+		string Line;
+
+		while (getline(File, Line))
+		{
+			cout << Line << endl;
+		}
+
+		File.close();
+	}
+}
 
 int main()
 {
-	vector <string> vFileContent{ "Ali", "Shadi", "Maher", "Fadi" };
+	cout << "File content before deletion\n";
 
-	SaveVectorToFile("SecondFile.txt", vFileContent);
+	PrintFileContent("SecondFile.txt");
+
+	
+	RemoveRecordFromVector("SecondFile.txt", "Ali");
+
+	cout << "\nFile content After deletion\n";
+
+	PrintFileContent("SecondFile.txt");
 
 	return 0;
 }
