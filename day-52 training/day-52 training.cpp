92a917fa3be4d893937d4266396267825c1eb29b
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-09-01 20:05:40 +0400

    local and utc time

--- day-52 training/day-52 training.cpp
+++ day-52 training/day-52 training.cpp
@@ -1,98 +1,19 @@
+#pragma warning(disable : 4996)
+
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <ctime>
 using namespace std;
-
-void LoadDataFromFileToVector(string FileName, vector<string>& vFileContent)
-{
-	fstream File;
-
-	File.open(FileName, ios::in);
-
-	if (File.is_open())
-	{
-		string Line;
-
-		while (getline(File, Line))
-		{
-			vFileContent.push_back(Line);
-		}
-
-		File.close();
-	}
-}
-
-void SaveVectorToFile(string FileName, vector <string> vFileContent)
-{
-	fstream File;
-
-	File.open(FileName, ios::out);
-
-	if (File.is_open())
-	{
-		for (string& Line : vFileContent)
-		{
-			if (Line != "") {
-
-				File << Line << endl;
-			}
-
-		}
-
-		File.close();
-	}
-}
-
-void UpdateRecordInFile(string FileName, string Record, string UpdateTo)
-{
-	vector<string> vFileContent;
-
-	LoadDataFromFileToVector(FileName, vFileContent);
-
-	for (string& Item : vFileContent)
-	{
-		if (Item == Record)
-		{
-			Item = UpdateTo;
-		}
-	}
-
-	SaveVectorToFile("SecondFile.txt", vFileContent);
-}
-
-
-void PrintFileContent(string FileName)
-{
-	fstream File;
-
-	File.open(FileName, ios::in);
-
-	if (File.is_open())
-	{
-		string Line;
-
-		while (getline(File, Line))
-		{
-			cout << Line << endl;
-		}
-
-		File.close();
-	}
-}
 
 int main()
 {
-	cout << "File content before update\n";
+	time_t t = time(0);
 
-	PrintFileContent("SecondFile.txt");
+	char* dt = ctime(&t);
+	cout << "Local date and time is: " << dt << "\n";
 
-	
-	UpdateRecordInFile("SecondFile.txt", "Ali", "Omar");
-
-	cout << "\nFile content After update\n";
-
-	PrintFileContent("SecondFile.txt");
-
+	tm* gmtm = gmtime(&t);
+	dt = asctime(gmtm);
+	cout << "UTC date and time is: " << dt << endl;
+		
 	return 0;
 } 
