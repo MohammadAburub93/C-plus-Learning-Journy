25c2b9bcce27b82407d070572aabf305791d097e
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-09-09 07:11:07 +0400

    palindrome matrix

---
+++ algorithms trainings level 3/palindrome matrix/palindrome matrix.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+
+int RandomNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+
+	return RandNum;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			arr[i][j] = RandomNumber(0, 1);
+		}
+	}
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++)
+		{
+			printf("%0*d   ", 2, arr[i][j]);
+		}
+
+		cout << endl;
+	}
+}
+
+bool IsPalindromeMatrix(int Mat1[3][3], short Rows, short Cols)
+{
+	short RowBackwardPosition;
+	for (int i = 0; i < Rows; i++)
+	{
+		RowBackwardPosition = Cols - 1;
+		for (int j = 0; j < Cols; j++)
+		{
+			if (Mat1[i][j] != Mat1[i][RowBackwardPosition])
+			{
+				return false;
+			}
+			RowBackwardPosition--;
+		}
+	}
+
+	return true;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Mat1[3][3]{ {1, 0, 1}, {4, 0, 4}, {5, 0, 5} };
+
+	//FillMatrixWithRandomNumbers(Mat1, 3, 3);
+
+	cout << "\nMatrix 1: \n\n";
+	PrintMatrix(Mat1, 3, 3);
+
+	if (IsPalindromeMatrix(Mat1, 3, 3))
+	{
+		cout << "\nYes: Matrix is Palindrome\n";
+	}
+	else
+	{
+		cout << "\nNo: Matrix is not Palindrome\n";
+	}
+
+	system("pause>0");
+
+	return 0;
+}
