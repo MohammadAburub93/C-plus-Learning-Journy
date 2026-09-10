f9e324ecd0e1e2b99627f4df1cea71dffe54c1f3
Mohammad Aburub <mohammad.t.aburub@gmail.com>
2026-09-10 10:23:17 +0400

    uppercase first letter of each word

---
+++ algorithms trainings level 3/uppercase first letter of each word in string/uppercase first letter of each word in string.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string ReadString()
+{
+	string Text;
+
+	cout << "Please enter your string? " << endl; Text;
+	getline(cin, Text);
+
+	return Text;
+}
+
+string UppercaseFirstLetterOfWord(string Text)
+{
+	bool IsFirstLetter = true;
+
+	for (short i = 0; i < Text.length(); i++)
+	{
+		if (Text[i] != ' ' && IsFirstLetter)
+		{
+			Text[i] = toupper(Text[i]);
+		}
+		IsFirstLetter = (Text[i] == ' ' ? true : false);
+	}
+
+	return Text;
+}
+
+int main()
+{
+	string Text = ReadString();
+
+	cout << "\nString after converging:\n\n";
+
+	Text = UppercaseFirstLetterOfWord(Text);
+
+	cout << Text << endl;
+
+	system("pause>0");
+
+	return 0;
+}
